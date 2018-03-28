aa4af915347b782ff9a1630473ec1cf3df489f08
Royce Remulla <royce.com@gmail.com>
2018-03-28 09:49:49 +0800

    Completed

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+
+[Step by Step](http://guides.rubyonrails.org/v3.2/getting_started.html)
+
+`$ rails _3.2.22.1_ new railsguide32`
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsguide32::Application.config.secret_token = 'd4fb9e276851eaa3b0c909014a91833e95e29dcc29e819f883147dbade7110b41e87d1745674eb9d8b356f38fd8cb5c0c322a0a7e4faca9fc71d9d3e28b39ad4'
